1c625b4a54584a25fb4550a4184ca07a709f8966
awele <awele.osuka@gmail.com>
2019-06-13 18:25:14 +0100

    fixed popup

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -1,3 +1,4 @@
+<link href="<?php echo base_url() ?>assets/css/simpleLightbox.css" rel="stylesheet" type="text/css" />
 <div class="banner-bottom gallery">
 		<div class="container">
 		<div class="wthree_head_section">
@@ -6,7 +7,7 @@
 			<div class="w3ls_gallery_grids">
 				<div class="col-md-4 w3_agile_gallery_grid">
 					<div class="agile_gallery_grid">
-						<a title="Donec sapien massa, placerat ac sodales ac, feugiat quis est." href="<?php echo base_url() ?>assets/images/1.jpg">
+						<a title="" href="<?php echo base_url() ?>assets/images/1.jpg">
 							<div class="agile_gallery_grid1">
 								<img src="<?php echo base_url() ?>assets/images/1.jpg" alt=" " class="img-responsive" />
 								
@@ -15,7 +16,7 @@
 					</div>
 					
 					<div class="agile_gallery_grid">
-						<a title="Donec sapien massa, placerat ac sodales ac, feugiat quis est." href="<?php echo base_url() ?>assets/images/2.jpg">
+						<a title="" href="<?php echo base_url() ?>assets/images/2.jpg">
 							<div class="agile_gallery_grid1">
 								<img src="<?php echo base_url() ?>assets/images/2.jpg" alt=" " class="img-responsive" />
 								
@@ -24,7 +25,7 @@
 					</div>
 
 					<div class="agile_gallery_grid">
-						<a title="Donec sapien massa, placerat ac sodales ac, feugiat quis est." href="<?php echo base_url() ?>assets/images/3.jpg">
+						<a title="" href="<?php echo base_url() ?>assets/images/3.jpg">
 							<div class="agile_gallery_grid1">
 								<img src="<?php echo base_url() ?>assets/images/3.jpg" alt=" " class="img-responsive" />
 								
@@ -42,7 +43,7 @@
 
 				<div class="col-md-4 w3_agile_gallery_grid">
 					<div class="agile_gallery_grid">
-						<a title="Donec sapien massa, placerat ac sodales ac, feugiat quis est." href="<?php echo base_url() ?>assets/images/4.jpg">
+						<a title="" href="<?php echo base_url() ?>assets/images/4.jpg">
 							<div class="agile_gallery_grid1">
 								<img src="<?php echo base_url() ?>assets/images/4.jpg" alt=" " class="img-responsive" />
 								
@@ -51,7 +52,7 @@
 					</div>
 					
 					<div class="agile_gallery_grid">
-						<a title="Donec sapien massa, placerat ac sodales ac, feugiat quis est." href="<?php echo base_url() ?>assets/images/5.jpeg">
+						<a title="" href="<?php echo base_url() ?>assets/images/5.jpeg">
 							<div class="agile_gallery_grid1">
 								<img src="<?php echo base_url() ?>assets/images/5.jpeg" alt=" " class="img-responsive" />
 								
@@ -60,7 +61,7 @@
 					</div>
 
 					<div class="agile_gallery_grid">
-						<a title="Donec sapien massa, placerat ac sodales ac, feugiat quis est." href="<?php echo base_url() ?>assets/images/6.jpeg">
+						<a title="" href="<?php echo base_url() ?>assets/images/6.jpeg">
 							<div class="agile_gallery_grid1">
 								<img src="<?php echo base_url() ?>assets/images/6.jpeg" alt=" " class="img-responsive" />
 								
@@ -72,7 +73,7 @@
 
 				<div class="col-md-4 w3_agile_gallery_grid">
 					<div class="agile_gallery_grid">
-						<a title="Donec sapien massa, placerat ac sodales ac, feugiat quis est." href="<?php echo base_url() ?>assets/images/7.jpeg">
+						<a title="" href="<?php echo base_url() ?>assets/images/7.jpeg">
 							<div class="agile_gallery_grid1">
 								<img src="<?php echo base_url() ?>assets/images/7.jpeg" alt=" " class="img-responsive" />
 								
@@ -81,7 +82,7 @@
 					</div>
 					
 					<div class="agile_gallery_grid">
-						<a title="Donec sapien massa, placerat ac sodales ac, feugiat quis est." href="<?php echo base_url() ?>assets/images/8.jpeg">
+						<a title="" href="<?php echo base_url() ?>assets/images/8.jpeg">
 							<div class="agile_gallery_grid1">
 								<img src="<?php echo base_url() ?>assets/images/8.jpeg" alt=" " class="img-responsive" />
 								
@@ -90,7 +91,7 @@
 					</div>
 
 					<div class="agile_gallery_grid">
-						<a title="Donec sapien massa, placerat ac sodales ac, feugiat quis est." href="<?php echo base_url() ?>assets/images/9.jpeg">
+						<a title="" href="<?php echo base_url() ?>assets/images/9.jpeg">
 							<div class="agile_gallery_grid1">
 								<img src="<?php echo base_url() ?>assets/images/9.jpeg" alt=" " class="img-responsive" />
 								
